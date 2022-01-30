729508922e48640d954a20bc77f3dcf56a17bc75
Denys Obelets <denamyte@gmail.com>
2022-01-30 13:59:44 +0200

    2:39:50

--- main.go
+++ main.go
@@ -1,18 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"booking-app/helper"
+	"fmt"
+	"strings"
+)
+
+var conferenceName = "Go Conference"
+
+const conferenceTickets uint = 50
+
+var remainingTickets uint = 50
+var bookings []string
 
 func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets uint8 = 50
-	var remainingTickets uint = 50
+	greetUsers()
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	for {
+		firstName, lastName, email, userTickets := getUserInput()
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			bookTicket(userTickets, firstName, lastName, email)
+
+			firstNames := getFirstNames()
+			fmt.Printf("The firsn names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short.")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign.")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid.")
+			}
+		}
+
+	}
+
+	//city := "London"
+	//
+	//switch city {
+	//	case "New York":
+	//		// execute code for booking New York conference tickets
+	//	case "Singapore", "Hong Kong":
+	//		// execute code for booking Singapore & Hong Kong conference tickets
+	//	case "London", "Berlin":
+	//		// some code here for London & Berlin
+	//	case "Mexico City":
+	//		// some code here
+	//	default:
+	//		fmt.Println("No valid city selected.")
+	//
+	//}
+
+}
+
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
@@ -30,10 +82,28 @@ func main() {
 
 	fmt.Print("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
+	return firstName, lastName, email, userTickets
+}
 
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking conference!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames() []string {
+	var firstNames []string
+	for _, booking := range bookings {
+		names := strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n")
-
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
